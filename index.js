451fc65200bced7400b5d5d2f6bb72d932d6826c
el-sam <samurai.h@yahoo.com>
2025-10-06 19:22:08 +0800

    npm test is ready. array is ready

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// code your solution here
+const record = [
+  { year: "2015", result: "W"},
+  { year: "2014", result: "N/A"},
+  { year: "2013", result: "L"},
+  //...
+]/ code your solution here

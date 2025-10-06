82ce7d96c47e956876dfa68a218d3d0793186dee
el-sam <samurai.h@yahoo.com>
2025-10-06 19:48:15 +0800

    lab complete

--- index.js
+++ index.js
@@ -3,4 +3,14 @@ const record = [
   { year: "2014", result: "N/A"},
   { year: "2013", result: "L"},
   //...
-]/ code your solution here
+]
+const finder = (element) => element.result === "W"
+function superbowlWin(arr) {
+    let op = arr.find(finder)
+    // if (op === true) {
+    //     console.log(op.year)   
+    // }
+    return op && op.year
+}
+
+superbowlWin(record)
